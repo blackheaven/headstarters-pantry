2e6cb4ef98396c7679e8e4349cfccd0b5fc50fc2
Gautier DI FOLCO <gautier.difolco@gmail.com>
2024-08-03 20:34:44 +0200

    missing nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "pantry";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs{ inherit system; };
+      in
+      {
+        defaultPackage = pkgs.hello;
+
+        devShell =
+          pkgs.mkShell {
+            buildInputs = [
+              # pkgs.psa
+              pkgs.nodejs
+              pkgs.nodePackages.typescript-language-server
+              # pkgs.yarn
+            ];
+          };
+      });
+}
